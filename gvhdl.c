3b0f64035753d9aeb28621fa3833fdec090a2833
fpfrimer <fpfirmer@gmail.com>
2023-03-25 18:49:34 -0300

    added menu

--- gvhdl.c
+++ gvhdl.c
@@ -12,6 +12,7 @@
     Prototypes
 *******************************************/
 char *replace_substring(const char *str, const char *old_substring, const char *new_substring);
+char print_main_menu(char *file_name);
 
 /*******************************************
     main
@@ -22,6 +23,7 @@ int main(int argc, char *argv[]) {
     char buffer[BUFFER_SIZE];       // Buffer to read lines
     char *lines[24];
     unsigned int line = 0;
+    char option;
     
 
     // Check if the correct number of arguments has been provided
@@ -34,23 +36,51 @@ int main(int argc, char *argv[]) {
     entity = argv[1];
     architecture = argv[2];
 
-    // Open file for reading
-    FILE *file = fopen(TEMPLATE_NAME, "r");
-    if (file == NULL) {
-        printf("Error opening the file '%s'.\n", TEMPLATE_NAME);
-        exit(EXIT_FAILURE);
-    }
+    // Menu
+    option = print_main_menu(entity);
+    while (1){
+        switch (option){
+        case 1: // Create file
+            // Open file for reading
+            FILE *file = fopen(TEMPLATE_NAME, "r");
+            if (file == NULL) {
+                printf("Error opening the file '%s'.\n", TEMPLATE_NAME);
+                exit(EXIT_FAILURE);
+            }
 
-    while (fgets(buffer, BUFFER_SIZE, file) != NULL) {
-        line = 0;
-        lines[line] = replace_substring(buffer, "<entity_name>", entity);
-        lines[line] = replace_substring(lines[line], "<architecture_name>", architecture);
-        printf("%s", lines[line]);
-        line++;        
-    }
+            while (fgets(buffer, BUFFER_SIZE, file) != NULL) {
+                line = 0;
+                lines[line] = replace_substring(buffer, "<entity_name>", entity);
+                lines[line] = replace_substring(lines[line], "<architecture_name>", architecture);
+                printf("%s", lines[line]);
+                line++;        
+            }
 
-    // Close the file
-    fclose(file); 
+            // Close the file
+            fclose(file);
+            return 0;
+            break;
+
+        case 2: // Kit DE10 Lite interfaces
+            printf("Still under development.\n");
+            break;
+
+        case 3: // Show template
+            printf("Still under development.\n");
+            break;
+
+        case 4: // Quit
+            printf("Good by!.\n");
+            return 0;
+            break;
+        
+        default:
+            return 1;
+            break;
+        }
+    }
+    
+        
 
     return 0;
 }
@@ -91,4 +121,27 @@ char *replace_substring(const char *str, const char *old_substring, const char *
     strcpy(result + i, str);
 
     return result;
+}
+
+/*  print_main_menu
+    The print_main_menu function prints the menu, reads the user input, and validates if the chosen option is between 1 and 4. If the option is invalid, the user will be prompted to enter again until a valid option is selected. The function returns the value of the chosen option as a char.
+
+*/
+char print_main_menu(char *file_name){
+    int choice;
+
+    printf("\nMenu:\n");
+    printf("1. Done! create file %s.vhd.\n", file_name);
+    printf("2. Kit DE10 Lite interfaces.\n");
+    printf("3. Show template.\n");
+    printf("4. Cancel and quit.\n");
+    printf("Enter the number of the chosen option: ");
+    scanf("%d", &choice);
+
+    while (choice < 1 || choice > 4) {
+        printf("Invalid option. Please, enter again: ");
+        scanf("%d", &choice);
+    }
+
+    return (char)choice;
 }
